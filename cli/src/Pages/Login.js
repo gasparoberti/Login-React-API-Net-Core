74d425363af5a506ebeef6c81358cf702b18fd78
Gaspar <obertigaspar@gmail.com>
2021-01-08 14:38:13 -0300

    se agregó para que muestre por consola el contenido que se va escribiendo en los imputs

--- cli/src/Pages/Login.js
+++ cli/src/Pages/Login.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import md5 from 'md5';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Cookies from 'universal-cookie';
@@ -6,6 +6,22 @@ import axios from 'axios';
 import '../Css/Login.css'; 
 
 function Login(props) {
+    const baseUrl="";
+
+    const [form, setForm] = useState({
+        username:'',    //deben tener el mismo valor que name en el input
+        password:''
+    })
+
+    const handleChange = e => {
+        const {name, value} = e.target;
+        setForm({
+            ...form,
+            [name]: value
+        })
+        console.log(form);
+    }
+
     return (
         <div className="containerPrincipal">
             <div className="containerLogin">
@@ -16,7 +32,7 @@ function Login(props) {
                     type="text"
                     className="form-control"
                     name="username"
-                    // onChange={handleChange}
+                    onChange={handleChange}
                     />
                     <br />
                     <label>Contraseña: </label>
@@ -25,7 +41,7 @@ function Login(props) {
                     type="password"
                     className="form-control"
                     name="password"
-                    // onChange={handleChange}
+                    onChange={handleChange}
                     />
                     <br />
                     {/* <button className="btn btn-primary" onClick={()=>iniciarSesion()}>Iniciar Sesión</button> */}
