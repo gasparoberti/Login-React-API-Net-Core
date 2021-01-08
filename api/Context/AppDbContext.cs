b8dc4d21bdba34afb19a09f247ed0d10d5ceefdf
Gaspar <obertigaspar@gmail.com>
2021-01-08 12:38:24 -0300

    first commit

---
+++ api/Context/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using api.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Usuario> usuarios { get; set; }
+    }
+}
